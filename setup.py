3546bf1fd7fb059b00802bb5f131b9148a04aea1
carolinesands <caroline.sands01@imperial.ac.uk>
2019-06-18 16:51:23 +0100

    New version release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='nPYc',
-	version='1.2.0',
+	version='1.2.1',
 	description='National Phenome Centre toolbox',
 	url='https://github.com/phenomecentre/npyc-toolbox',
 	author='National Phenome Centre',
