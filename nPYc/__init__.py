614339d7b1fc7c82e7eaec1bb3710822ca9ca84d
gscorreia89 <gscorreia89@gmail.com>
2021-08-10 13:32:38 +0100

    - update version

--- nPYc/__init__.py
+++ nPYc/__init__.py
@@ -1,7 +1,7 @@
 """
 The `nPYc-Toolbox <https://github.com/phenomecentre/nPYc-Toolbox>`_ defines objects for representing, and implements functions to manipulate and display, metabolic profiling datasets.
 """
-__version__ = '1.2.7'
+__version__ = '1.2.6'
 
 from . import enumerations
 from .objects import Dataset, MSDataset, NMRDataset, TargetedDataset
